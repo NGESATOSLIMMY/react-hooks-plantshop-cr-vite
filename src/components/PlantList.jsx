d1e118aee2779af033d0e13bb7da2280b2fe195c
NGESATOSLIMMY <emmanuelngesa0@gmail.com>
2026-08-11 10:14:34 +0300

    fix: update price display format in PlantCard component

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -1,26 +1,14 @@
-function PlantPage() {
-  const [plants, setPlants] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+import React from "react";
+import PlantCard from "./PlantCard";
 
-  useEffect(() => {
-    fetch("http://localhost:6001/plants")
-      .then((res) => res.json())
-      .then((data) => setPlants(data));
-  }, []);
-
-  function handleNewPlant(newPlant) {
-    setPlants((prevPlants) => [...prevPlants, newPlant]);
-  }
-
-  const filteredPlants = plants.filter((plant) =>
-    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
+function PlantList({ plants }) {
   return (
-    <main>
-      <NewPlantForm onAddPlant={handleNewPlant} />
-      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
-      <PlantList plants={filteredPlants} />
-    </main>
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard key={plant.id} plant={plant} />
+      ))}
+    </ul>
   );
 }
+
+export default PlantList;
